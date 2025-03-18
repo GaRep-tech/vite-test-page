7624bdb68a7c87c6d43e53a4a5b92fed91f928b7
Galen <galenreposh@gmail.com>
2025-03-18 18:55:59 -0400

    third commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import './style.css'
 import * as THREE from 'three'
 
 import { defineConfig } from 'vite'
-    import react from '@vitejs/plugin-react'
+  //import react from '@vitejs/plugin-react'
     
     // https://vitejs.dev/config/
     export default defineConfig({
-      plugins: [react()],
+     // plugins: [react()],
       base: "/vite-test-page/"
     })
 
